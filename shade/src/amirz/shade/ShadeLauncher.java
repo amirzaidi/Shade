ac5e4b2581a8086adfa3563bd7d311d11eaf6afb
Amir Zaidi <azaidi@live.nl>
2018-10-10 21:07:31 +0200

    Override themes with custom shade version

--- shade/src/amirz/shade/ShadeLauncher.java
+++ shade/src/amirz/shade/ShadeLauncher.java
@@ -1,7 +1,18 @@
 package amirz.shade;
 
 import com.android.launcher3.Launcher;
+import com.android.launcher3.R;
+import com.android.launcher3.uioverrides.WallpaperColorInfo;
 
 public class ShadeLauncher extends Launcher {
-
+    @Override
+    protected int getThemeRes(WallpaperColorInfo wallpaperColorInfo) {
+        if (wallpaperColorInfo.isDark()) {
+            return wallpaperColorInfo.supportsDarkText() ?
+                    R.style.LauncherTheme_Dark_DarkText_Shade : R.style.LauncherTheme_Dark_Shade;
+        } else {
+            return wallpaperColorInfo.supportsDarkText() ?
+                    R.style.LauncherTheme_DarkText_Shade : R.style.LauncherTheme_Shade;
+        }
+    }
 }
