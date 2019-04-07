d6218a6fe587ba6837a04ab01bb3dee628d4f870
Amir Zaidi <azaidi@live.nl>
2019-04-07 17:54:42 +0200

    Manually set the scale of adaptive category icons

--- shade/src/amirz/shade/allapps/CategoryShortcuts.java
+++ shade/src/amirz/shade/allapps/CategoryShortcuts.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
+import android.graphics.RectF;
+import android.graphics.drawable.AdaptiveIconDrawable;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.LayerDrawable;
 import android.os.Build;
@@ -14,7 +17,9 @@ import com.android.launcher3.AppInfo;
 import com.android.launcher3.Launcher;
 import com.android.launcher3.R;
 import com.android.launcher3.Utilities;
+import com.android.launcher3.graphics.FixedScaleDrawable;
 import com.android.launcher3.graphics.LauncherIcons;
+import com.android.launcher3.util.Themes;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,15 +39,26 @@ class CategoryShortcuts {
             Drawable d = res.getDrawable(icons.getResourceId(i, 0), launcher.getTheme());
             Bitmap bm;
             if (Utilities.ATLEAST_OREO) {
-                bm = launcherIcons.createBadgedIconBitmap(d, Process.myUserHandle(),
-                        Build.VERSION.SDK_INT, false, null).icon;
+                AdaptiveIconDrawable dr = (AdaptiveIconDrawable)
+                        launcher.getDrawable(R.drawable.adaptive_icon_drawable_wrapper).mutate();
+                dr.setBounds(0, 0, 1, 1);
+                float scale = launcherIcons.getNormalizer().getScale(
+                        dr, new RectF(), dr.getIconMask(), new boolean[1]);
+                FixedScaleDrawable fsd = ((FixedScaleDrawable) dr.getForeground());
+                fsd.setDrawable(d);
+                fsd.setScale(scale);
+                ((ColorDrawable) dr.getBackground()).setColor(
+                        Themes.getAttrColor(launcher, R.attr.headerColor));
+                d = dr;
             } else {
                 LayerDrawable circle = (LayerDrawable) res.getDrawable(
                         R.drawable.ic_category_plate, launcher.getTheme());
                 circle.setDrawableByLayerId(R.id.ic_category_plate_icon, d);
-                bm = launcherIcons.createBadgedIconBitmap(circle, Process.myUserHandle(),
-                        Build.VERSION.SDK_INT, false, null).icon;
+                d = circle;
             }
+
+            bm = launcherIcons.createBadgedIconBitmap(d, Process.myUserHandle(),
+                    Build.VERSION.SDK_INT, false, null).icon;
 
             AppInfo ai = new AppInfo();
             ai.componentName = cn;
@@ -54,6 +70,7 @@ class CategoryShortcuts {
             categories.add(ai);
         }
         icons.recycle();
+        launcherIcons.recycle();
 
         return categories;
     }
