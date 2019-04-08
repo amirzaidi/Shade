400e221fd4797defe37591ef1a7e673b5d905761
Amir Zaidi <azaidi@live.nl>
2019-04-08 21:43:30 +0200

    Separate icon loading by ComponentKey in IconProvider for static use

--- shade/src/amirz/shade/icons/ThirdPartyIconProvider.java
+++ shade/src/amirz/shade/icons/ThirdPartyIconProvider.java
@@ -4,12 +4,9 @@ import android.content.Context;
 import android.content.pm.LauncherActivityInfo;
 import android.graphics.drawable.Drawable;
 
-import com.android.launcher3.IconCache;
-import com.android.launcher3.LauncherAppState;
 import com.android.launcher3.Utilities;
 import com.android.launcher3.util.ComponentKey;
 
-import amirz.shade.icons.calendar.DateChangeReceiver;
 import amirz.shade.icons.calendar.DynamicCalendar;
 import amirz.shade.icons.clock.CustomClock;
 import amirz.shade.icons.clock.DynamicClock;
@@ -18,12 +15,10 @@ import amirz.shade.icons.pack.IconResolver;
 
 public class ThirdPartyIconProvider extends RoundIconProvider {
     private final Context mContext;
-    private final IconPackManager mManager;
 
     public ThirdPartyIconProvider(Context context) {
         super(context);
         mContext = context;
-        mManager = IconPackManager.get(context);
     }
 
     @Override
@@ -31,7 +26,14 @@ public class ThirdPartyIconProvider extends RoundIconProvider {
         ComponentKey key = new ComponentKey(
                 launcherActivityInfo.getComponentName(), launcherActivityInfo.getUser());
 
-        IconResolver resolver = mManager.resolve(key);
+        Drawable icon = getByKey(mContext, key, iconDpi);
+        return icon == null
+                ? super.getIcon(launcherActivityInfo, iconDpi, flattenDrawable)
+                : icon;
+    }
+
+    static Drawable getByKey(Context context, ComponentKey key, int iconDpi) {
+        IconResolver resolver = IconPackManager.get(context).resolve(key);
         Drawable icon = resolver == null
                 ? null
                 : resolver.getIcon(iconDpi);
@@ -39,24 +41,20 @@ public class ThirdPartyIconProvider extends RoundIconProvider {
         if (Utilities.ATLEAST_OREO) {
             // Icon pack clock found
             if (icon != null && resolver.isClock()) {
-                return CustomClock.getClock(mContext, icon, resolver.clockData());
+                return CustomClock.getClock(context, icon, resolver.clockData());
             }
 
             // Google Clock override
-            if (icon == null && launcherActivityInfo.getComponentName()
-                    .equals(DynamicClock.DESK_CLOCK)) {
-                icon = DynamicClock.getClock(mContext, iconDpi);
+            if (icon == null && key.componentName.equals(DynamicClock.DESK_CLOCK)) {
+                return DynamicClock.getClock(context, iconDpi);
             }
         }
 
         // Google Calendar override
-        if (icon == null && launcherActivityInfo.getComponentName().getPackageName()
-                .equals(DynamicCalendar.CALENDAR)) {
-            icon = DynamicCalendar.load(mContext, launcherActivityInfo.getComponentName(), iconDpi);
+        if (icon == null && key.componentName.getPackageName().equals(DynamicCalendar.CALENDAR)) {
+            return DynamicCalendar.load(context, key.componentName, iconDpi);
         }
 
-        return icon == null
-                ? super.getIcon(launcherActivityInfo, iconDpi, flattenDrawable)
-                : icon;
+        return icon;
     }
 }
