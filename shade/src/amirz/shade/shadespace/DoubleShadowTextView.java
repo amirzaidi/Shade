01c6289e9638277f1ae9ccb3ed4ee187edc2b1b6
Amir Zaidi <azaidi@live.nl>
2019-04-09 21:59:04 +0200

    Resize Shade Space text when it does not fit by up to 15%

--- shade/src/amirz/shade/shadespace/DoubleShadowTextView.java
+++ shade/src/amirz/shade/shadespace/DoubleShadowTextView.java
@@ -3,12 +3,15 @@ package amirz.shade.shadespace;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.widget.TextView;
 
 import com.android.launcher3.views.DoubleShadowBubbleTextView;
 
 public class DoubleShadowTextView extends TextView {
+    private static final float MIN_SHRINK = 0.85f;
     private final DoubleShadowBubbleTextView.ShadowInfo mShadowInfo;
+    private float mTextSize = Float.NaN;
 
     public DoubleShadowTextView(Context context) {
         this(context, null);
@@ -38,5 +41,33 @@ public class DoubleShadowTextView extends TextView {
                     0f, mShadowInfo.keyShadowOffset, mShadowInfo.keyShadowColor);
             super.onDraw(canvas);
         }
+    }
+
+    @Override
+    protected void onTextChanged(CharSequence text, int start, int lengthBefore, int lengthAfter) {
+        super.onTextChanged(text, start, lengthBefore, lengthAfter);
+        if (getWidth() > 0) {
+            resizeText();
+        }
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if (getWidth() > 0) {
+            resizeText();
+        }
+    }
+
+    private void resizeText() {
+        if (Float.isNaN(mTextSize)) {
+            mTextSize = getTextSize();
+        } else {
+            setTextSize(TypedValue.COMPLEX_UNIT_PX, mTextSize);
+        }
+
+        int w = getWidth() - getTotalPaddingLeft() - getTotalPaddingRight();
+        float ratio = Math.min(1f, w / getPaint().measureText(getText().toString()));
+        setTextSize(TypedValue.COMPLEX_UNIT_PX, mTextSize * Math.max(MIN_SHRINK, ratio));
     }
 }
