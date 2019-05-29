372f4292fb38a236506dbf31f324dc9d48f6ba61
Amir Zaidi <azaidi@live.nl>
2019-05-29 14:27:57 +0200

    Show package and activity in long press preferences on debug builds

--- shade/src/amirz/shade/customization/PreferencesBottomSheet.java
+++ shade/src/amirz/shade/customization/PreferencesBottomSheet.java
@@ -2,6 +2,8 @@ package amirz.shade.customization;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.os.Bundle;
@@ -10,7 +12,9 @@ import android.preference.PreferenceFragment;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.launcher3.BuildConfig;
 import com.android.launcher3.ItemInfo;
 import com.android.launcher3.Launcher;
 import com.android.launcher3.R;
@@ -67,6 +71,9 @@ public class PreferencesBottomSheet extends WidgetsSheet {
     }
 
     public static class PrefsFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+        private final static String PREF_DEBUG_PACKAGE = "pref_debug_package";
+        private final static String PREF_DEBUG_ACTIVITY = "pref_debug_activity";
+
         private final static String PREF_ICON = "pref_app_icon";
         private final static String PREF_CATEGORY = "pref_app_category";
         private final static String PREF_RESET = "pref_app_reset";
@@ -85,12 +92,35 @@ public class PreferencesBottomSheet extends WidgetsSheet {
             super.onCreate(savedInstanceState);
             mContext = getActivity();
 
+            if (BuildConfig.DEBUG) {
+                addPreferencesFromResource(R.xml.app_edit_prefs_debug);
+            }
             addPreferencesFromResource(R.xml.app_edit_prefs);
         }
 
         public void loadForApp(ItemInfo itemInfo, final View.OnClickListener onResetClick) {
             mComponent = itemInfo.getTargetComponent();
             mKey = new ComponentKey(mComponent, itemInfo.user);
+
+            if (BuildConfig.DEBUG) {
+                ClipboardManager clipboard =
+                        (ClipboardManager) mContext.getSystemService(Context.CLIPBOARD_SERVICE);
+
+                Preference.OnPreferenceClickListener onClick = p -> {
+                    ClipData data = ClipData.newPlainText("pref", mComponent.flattenToString());
+                    clipboard.setPrimaryClip(data);
+                    Toast.makeText(mContext, R.string.pref_debug_copy, Toast.LENGTH_SHORT).show();
+                    return false;
+                };
+
+                Preference pkg = findPreference(PREF_DEBUG_PACKAGE);
+                pkg.setSummary(mComponent.getPackageName());
+                pkg.setOnPreferenceClickListener(onClick);
+
+                Preference act = findPreference(PREF_DEBUG_ACTIVITY);
+                act.setSummary(mComponent.getClassName());
+                act.setOnPreferenceClickListener(onClick);
+            }
 
             mPrefIcon = (IconPackPreference) findPreference(PREF_ICON);
             mPrefIcon.setValue(CustomizationDatabase.getIconPack(mContext, mKey));
