17256a3d30698b61f7cda0cd00fc44f92c9cad0a
Amir Zaidi <azaidi@live.nl>
2019-07-02 14:49:17 +0200

    Launcher3: Cleanup duplicate code from merge in NotificationListener

--- src/com/android/launcher3/notification/NotificationListener.java
+++ src/com/android/launcher3/notification/NotificationListener.java
@@ -379,12 +379,6 @@ public class NotificationListener extends NotificationListenerService {
                 if ((notification.flags & Notification.FLAG_ONGOING_EVENT) != 0) {
                     return true;
                 }
-                if (mTempRanking.getChannel().getId().equals(NotificationChannel.DEFAULT_CHANNEL_ID)) {
-                    // Special filtering for the default, legacy "Miscellaneous" channel.
-                    if ((notification.flags & Notification.FLAG_ONGOING_EVENT) != 0) {
-                        return true;
-                    }
-                }
             }
         }
 
