366e7dd43783bed7cfbdee049e07c9e13c959ba2
Amir Zaidi <azaidi@live.nl>
2020-01-05 17:50:31 +0100

    Shade: Do not show ShadeLauncher activity in app drawer

---
+++ shade/src/amirz/shade/ShadeAppFilter.java
@@ -0,0 +1,22 @@
+package amirz.shade;
+
+import android.content.ComponentName;
+import android.content.Context;
+
+import com.android.launcher3.AppFilter;
+import com.android.launcher3.BuildConfig;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ShadeAppFilter extends AppFilter {
+    private final Set<ComponentName> mFilter = new HashSet<>();
+
+    public ShadeAppFilter(Context context) {
+        mFilter.add(new ComponentName(BuildConfig.APPLICATION_ID, ShadeLauncher.class.getName()));
+    }
+
+    public boolean shouldShowApp(ComponentName app) {
+        return !mFilter.contains(app);
+    }
+}
