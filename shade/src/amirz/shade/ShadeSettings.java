27c5738476369292dbefb7330e8f761cccadab03
Amir Zaidi <azaidi@live.nl>
2020-01-06 00:08:32 +0100

    Shade: Remove space on the left side of Home Settings

--- shade/src/amirz/shade/ShadeSettings.java
+++ shade/src/amirz/shade/ShadeSettings.java
@@ -2,6 +2,9 @@ package amirz.shade;
 
 import android.os.Bundle;
 
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+
 import com.android.launcher3.settings.SettingsActivity;
 
 public class ShadeSettings extends SettingsActivity {
@@ -12,6 +15,15 @@ public class ShadeSettings extends SettingsActivity {
     }
 
     public static class ShadeSettingsFragment extends LauncherSettingsFragment {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            super.onCreatePreferences(savedInstanceState, rootKey);
 
+            PreferenceScreen screen = getPreferenceScreen();
+            for (int i = 0; i < screen.getPreferenceCount(); i++) {
+                Preference preference = screen.getPreference(i);
+                preference.setIconSpaceReserved(false);
+            }
+        }
     }
 }
