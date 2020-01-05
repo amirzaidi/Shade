8310782fad84f45895c7fd8a7a70a8473e314a71
Amir Zaidi <azaidi@live.nl>
2020-01-05 19:45:04 +0100

    Shade: Change ServiceIntent inside LauncherClient to use AIDL Bridge

---
+++ shade/src/amirz/aidlbridge/LauncherClientIntent.java
@@ -0,0 +1,36 @@
+package amirz.aidlbridge;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Process;
+
+import com.android.launcher3.BuildConfig;
+import com.google.android.libraries.gsa.launcherclient.BuildInfo;
+
+public class LauncherClientIntent {
+    private final static String PASSTHROUGH = "com.google.android.googlequicksearchbox";
+    private final static String BRIDGE = "amirz.aidlbridge";
+    public final static String DEFAULT = BuildConfig.DEBUG ? PASSTHROUGH : BRIDGE;
+
+    public static void setPackage(String pkg) {
+        // No-op
+    }
+
+    @SuppressWarnings("StringBufferReplaceableByString")
+    public static Intent getServiceIntent(Context context) {
+        String pkg = context.getPackageName();
+        return new Intent("com.android.launcher3.WINDOW_OVERLAY")
+                .setPackage(DEFAULT)
+                .setData(Uri.parse(new StringBuilder(pkg.length() + 18)
+                        .append("app://")
+                        .append(pkg)
+                        .append(":")
+                        .append(Process.myUid())
+                        .toString())
+                        .buildUpon()
+                        .appendQueryParameter("v", Integer.toString(BuildInfo.SERVER_VERSION_CODE))
+                        .appendQueryParameter("cv", Integer.toString(BuildInfo.CLIENT_VERSION_CODE))
+                        .build());
+    }
+}
