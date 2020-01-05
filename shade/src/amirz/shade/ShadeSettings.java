107cc74ab856f3e7e5499a63aba77aee788ba33b
Amir Zaidi <azaidi@live.nl>
2020-01-05 17:19:38 +0100

    Shade: Always apply Google Sans in ShadeSettings

--- shade/src/amirz/shade/ShadeSettings.java
+++ shade/src/amirz/shade/ShadeSettings.java
@@ -1,6 +1,13 @@
 package amirz.shade;
 
+import android.os.Bundle;
+
 import com.android.launcher3.settings.SettingsActivity;
 
 public class ShadeSettings extends SettingsActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        ShadeFont.override(this);
+        super.onCreate(savedInstanceState);
+    }
 }
