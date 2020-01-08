35fcdb9cf0c5508242599c815038c428ad637a5c
Amir Zaidi <azaidi@live.nl>
2020-01-08 23:38:19 +0100

    Shade: Import UsageTracker from Shade Pie

---
+++ shade/src_ui_overrides/amirz/shade/appprediction/UsageTracker.java
@@ -0,0 +1,74 @@
+package amirz.shade.appprediction;
+
+import android.annotation.SuppressLint;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+
+import com.android.launcher3.BuildConfig;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+class UsageTracker {
+    private final PackageManager mPm;
+    private final UsageStatsManager mManager;
+
+    @SuppressLint("WrongConstant")
+    UsageTracker(Context context) {
+        mPm = context.getPackageManager();
+        mManager = (UsageStatsManager) context.getSystemService("usagestats");
+    }
+
+    List<ComponentName> getSortedComponents() {
+        List<ComponentName> components = new ArrayList<>();
+        for (String pkg : getSortedPackages()) {
+            Intent intent = mPm.getLaunchIntentForPackage(pkg);
+            if (intent != null) {
+                components.add(intent.getComponent());
+            }
+        }
+        return components;
+    }
+
+    private List<String> getSortedPackages() {
+        List<String> packages = new ArrayList<>();
+        for (UsageStats stat : getSortedStats()) {
+            packages.add(stat.getPackageName());
+        }
+        return packages;
+    }
+
+    private List<UsageStats> getSortedStats() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.YEAR, -1);
+
+        List<UsageStats> stats = mManager.queryUsageStats(UsageStatsManager.INTERVAL_YEARLY,
+                calendar.getTimeInMillis(), System.currentTimeMillis());
+        SortedMap<Long, UsageStats> sorted = new TreeMap<>();
+        for (UsageStats usageStats : stats) {
+            sorted.put(-usageStats.getLastTimeUsed(), usageStats);
+        }
+
+        Set<String> packages = new HashSet<>();
+        packages.add(BuildConfig.APPLICATION_ID);
+        List<UsageStats> sortedList = new ArrayList<>();
+        for (UsageStats stat : sorted.values()) {
+            String pkg = stat.getPackageName();
+            if (!packages.contains(pkg)) {
+                packages.add(pkg);
+                sortedList.add(stat);
+            }
+        }
+
+        return sortedList;
+    }
+}
