58be607724cca70077fced577cc4e6bf8b381462
Amir Zaidi <azaidi@live.nl>
2020-01-09 18:01:36 +0100

    Shade: Disable prediction in FilteredPredictor based on preference

--- shade/src_ui_overrides/amirz/shade/appprediction/FilteredPredictor.java
+++ shade/src_ui_overrides/amirz/shade/appprediction/FilteredPredictor.java
@@ -15,6 +15,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import amirz.shade.settings.PredictionPreference;
+
 import static android.content.pm.PackageManager.GET_RESOLVED_FILTER;
 
 class FilteredPredictor extends UsageTracker {
@@ -27,14 +29,19 @@ class FilteredPredictor extends UsageTracker {
         FILTER_META = Collections.unmodifiableSet(filterMeta);
     }
 
+    private final Context mContext;
     private final int mCount;
 
     FilteredPredictor(Context context, int count) {
         super(context);
+        mContext = context;
         mCount = count;
     }
 
     List<ComponentName> getFilteredComponents() {
+        if (!PredictionPreference.isEnabled(mContext)) {
+            return Collections.emptyList();
+        }
         List<ComponentName> components = getSortedComponents();
         for (int i = components.size() - 1; i >= 0; i--) {
             if (shouldFilterComponent(components.get(i))) {
