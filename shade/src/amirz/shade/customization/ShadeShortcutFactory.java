f4425f4b183494bfb53a045d4501fa359e926a9a
Amir Zaidi <azaidi@live.nl>
2020-01-09 22:59:57 +0100

    Shade: Override AppInfoShortcut with BottomSheetShortcut

--- shade/src/amirz/shade/customization/ShadeShortcutFactory.java
+++ shade/src/amirz/shade/customization/ShadeShortcutFactory.java
@@ -1,13 +1,41 @@
 package amirz.shade.customization;
 
+import android.app.ActivityOptions;
+import android.graphics.Rect;
+import android.view.View;
+
+import com.android.launcher3.BaseDraggingActivity;
+import com.android.launcher3.ItemInfo;
+import com.android.launcher3.R;
 import com.android.launcher3.popup.SystemShortcut;
 import com.android.launcher3.popup.SystemShortcutFactory;
+import com.android.launcher3.userevent.nano.LauncherLogProto;
+import com.android.launcher3.util.PackageManagerHelper;
 
 @SuppressWarnings("unused")
 public class ShadeShortcutFactory extends SystemShortcutFactory {
     public ShadeShortcutFactory() {
-        super(new SystemShortcut.AppInfo(),
+        super(new BottomSheetShortcut(),
                 new SystemShortcut.Widgets(),
                 new SystemShortcut.Install());
+    }
+
+    public static class BottomSheetShortcut extends SystemShortcut {
+        public BottomSheetShortcut() {
+            super(R.drawable.ic_info_no_shadow, R.string.app_info_drop_target_label);
+        }
+
+        @Override
+        public View.OnClickListener getOnClickListener(
+                BaseDraggingActivity activity, ItemInfo itemInfo) {
+            return (view) -> {
+                dismissTaskMenuView(activity);
+            Rect sourceBounds = activity.getViewBounds(view);
+            new PackageManagerHelper(activity).startDetailsActivityForInfo(
+                    itemInfo, sourceBounds, ActivityOptions.makeBasic().toBundle());
+            activity.getUserEventDispatcher().logActionOnControl(LauncherLogProto.Action.Touch.TAP,
+                    LauncherLogProto.ControlType.APPINFO_TARGET, view);
+            };
+        }
     }
 }
