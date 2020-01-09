d9272148b338d63118e65af4a3477753c2a7c88e
Amir Zaidi <azaidi@live.nl>
2020-01-09 23:09:06 +0100

    Shade: Import bottom sheet class from Pie

---
+++ shade/src/amirz/shade/customization/InfoBottomSheet.java
@@ -0,0 +1,95 @@
+package amirz.shade.customization;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.launcher3.ItemInfo;
+import com.android.launcher3.Launcher;
+import com.android.launcher3.R;
+import com.android.launcher3.util.ComponentKey;
+import com.android.launcher3.widget.WidgetsBottomSheet;
+
+public class InfoBottomSheet extends WidgetsBottomSheet {
+    private final FragmentManager mFragmentManager;
+    private View.OnClickListener mOnAppInfoClick;
+
+    public InfoBottomSheet(Context context) {
+        this(context, null);
+    }
+
+    public InfoBottomSheet(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public InfoBottomSheet(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mFragmentManager = Launcher.getLauncher(context).getFragmentManager();
+    }
+
+    public void setOnAppInfoClick(View.OnClickListener onclick) {
+        mOnAppInfoClick = onclick;
+    }
+
+    @Override
+    public void populateAndShow(ItemInfo itemInfo) {
+        super.populateAndShow(itemInfo);
+        TextView title = findViewById(R.id.title);
+        title.setText(itemInfo.title);
+
+        View.OnClickListener l = v -> mOnAppInfoClick.onClick(v);
+        title.setOnClickListener(l);
+        findViewById(R.id.subtitle).setOnClickListener(l);
+
+        PrefsFragment fragment =
+                (PrefsFragment) mFragmentManager.findFragmentById(R.id.sheet_prefs);
+        fragment.loadForApp(itemInfo, v -> handleClose(true));
+    }
+
+    @Override
+    public void onDetachedFromWindow() {
+        Fragment pf = mFragmentManager.findFragmentById(R.id.sheet_prefs);
+        if (pf != null) {
+            mFragmentManager.beginTransaction()
+                    .remove(pf)
+                    .commitAllowingStateLoss();
+        }
+        super.onDetachedFromWindow();
+    }
+
+    @Override
+    public void onWidgetsBound() {
+    }
+
+    public static class PrefsFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+        private Context mContext;
+
+        private ComponentName mComponent;
+        private ComponentKey mKey;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            mContext = getActivity();
+        }
+
+        public void loadForApp(ItemInfo itemInfo, final View.OnClickListener onResetClick) {
+            mComponent = itemInfo.getTargetComponent();
+            mKey = new ComponentKey(mComponent, itemInfo.user);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            switch (preference.getKey()) {
+            }
+            return false;
+        }
+    }
+}
--- shade/src/amirz/shade/customization/ShadeShortcutFactory.java
+++ shade/src/amirz/shade/customization/ShadeShortcutFactory.java
@@ -1,16 +1,14 @@
 package amirz.shade.customization;
 
-import android.app.ActivityOptions;
-import android.graphics.Rect;
+import android.content.Context;
 import android.view.View;
 
 import com.android.launcher3.BaseDraggingActivity;
 import com.android.launcher3.ItemInfo;
+import com.android.launcher3.Launcher;
 import com.android.launcher3.R;
 import com.android.launcher3.popup.SystemShortcut;
 import com.android.launcher3.popup.SystemShortcutFactory;
-import com.android.launcher3.userevent.nano.LauncherLogProto;
-import com.android.launcher3.util.PackageManagerHelper;
 
 @SuppressWarnings("unused")
 public class ShadeShortcutFactory extends SystemShortcutFactory {
@@ -20,21 +18,27 @@ public class ShadeShortcutFactory extends SystemShortcutFactory {
                 new SystemShortcut.Install());
     }
 
-    public static class BottomSheetShortcut extends SystemShortcut {
-        public BottomSheetShortcut() {
-            super(R.drawable.ic_info_no_shadow, R.string.app_info_drop_target_label);
-        }
-
+    public static class BottomSheetShortcut extends SystemShortcut.AppInfo {
         @Override
         public View.OnClickListener getOnClickListener(
                 BaseDraggingActivity activity, ItemInfo itemInfo) {
-            return (view) -> {
-                dismissTaskMenuView(activity);
-            Rect sourceBounds = activity.getViewBounds(view);
-            new PackageManagerHelper(activity).startDetailsActivityForInfo(
-                    itemInfo, sourceBounds, ActivityOptions.makeBasic().toBundle());
-            activity.getUserEventDispatcher().logActionOnControl(LauncherLogProto.Action.Touch.TAP,
-                    LauncherLogProto.ControlType.APPINFO_TARGET, view);
+            Launcher launcher = (Launcher) activity;
+            final View.OnClickListener aiListener = super.getOnClickListener(launcher, itemInfo);
+            return new View.OnClickListener() {
+                private InfoBottomSheet cbs;
+
+                @Override
+                public void onClick(View v) {
+                    if (cbs == null) {
+                        dismissTaskMenuView(launcher);
+                        cbs = (InfoBottomSheet) launcher.getLayoutInflater().inflate(
+                                R.layout.app_info_bottom_sheet,
+                                launcher.getDragLayer(),
+                                false);
+                        cbs.setOnAppInfoClick(aiListener);
+                        cbs.populateAndShow(itemInfo);
+                    }
+                }
             };
         }
     }
