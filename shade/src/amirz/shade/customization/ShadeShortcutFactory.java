859427b43f25c7990c843cbb028cf07a47a2e451
Amir Zaidi <azaidi@live.nl>
2020-01-12 22:49:04 +0100

    Shade: Use app opening transition when using More app info shortcut

--- shade/src/amirz/shade/customization/ShadeShortcutFactory.java
+++ shade/src/amirz/shade/customization/ShadeShortcutFactory.java
@@ -1,14 +1,17 @@
 package amirz.shade.customization;
 
 import android.content.Context;
+import android.graphics.Rect;
+import android.os.Bundle;
 import android.view.View;
 
-import com.android.launcher3.BaseDraggingActivity;
 import com.android.launcher3.ItemInfo;
 import com.android.launcher3.Launcher;
 import com.android.launcher3.R;
 import com.android.launcher3.popup.SystemShortcut;
 import com.android.launcher3.popup.SystemShortcutFactory;
+import com.android.launcher3.userevent.nano.LauncherLogProto;
+import com.android.launcher3.util.PackageManagerHelper;
 
 @SuppressWarnings("unused")
 public class ShadeShortcutFactory extends SystemShortcutFactory {
@@ -18,12 +21,14 @@ public class ShadeShortcutFactory extends SystemShortcutFactory {
                 new SystemShortcut.Install());
     }
 
-    public static class BottomSheetShortcut extends SystemShortcut.AppInfo {
+    private static class BottomSheetShortcut extends SystemShortcut<Launcher> {
+        private BottomSheetShortcut() {
+            super(R.drawable.ic_info_no_shadow, R.string.app_info_drop_target_label);
+        }
+
         @Override
-        public View.OnClickListener getOnClickListener(
-                BaseDraggingActivity activity, ItemInfo itemInfo) {
-            Launcher launcher = (Launcher) activity;
-            final View.OnClickListener aiListener = super.getOnClickListener(launcher, itemInfo);
+        public View.OnClickListener getOnClickListener(Launcher launcher, ItemInfo itemInfo) {
+            final View.OnClickListener onClickMore = v -> onClickMore(launcher, itemInfo, v);
             return new View.OnClickListener() {
                 private InfoBottomSheet cbs;
 
@@ -35,11 +40,22 @@ public class ShadeShortcutFactory extends SystemShortcutFactory {
                                 R.layout.app_info_bottom_sheet,
                                 launcher.getDragLayer(),
                                 false);
-                        cbs.setOnAppInfoClick(aiListener);
+                        cbs.setOnAppInfoClick(onClickMore);
                         cbs.populateAndShow(itemInfo);
                     }
                 }
             };
+        }
+
+        private void onClickMore(Launcher launcher, ItemInfo itemInfo, View view) {
+            dismissTaskMenuView(launcher);
+            Rect sourceBounds = launcher.getViewBounds(view);
+            Bundle opts = launcher.getAppTransitionManager()
+                    .getActivityLaunchOptions(launcher, view).toBundle();
+            new PackageManagerHelper(launcher).startDetailsActivityForInfo(
+                    itemInfo, sourceBounds, opts);
+            launcher.getUserEventDispatcher().logActionOnControl(LauncherLogProto.Action.Touch.TAP,
+                    LauncherLogProto.ControlType.APPINFO_TARGET, view);
         }
     }
 }
