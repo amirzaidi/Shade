fe648c5a6544af742f023cc65da5ea6aefc1a376
Amir Zaidi <azaidi@live.nl>
2020-02-23 17:12:22 +0100

    Shade: Reveal hidden apps when searching for the word "Hidden"

--- shade/src/amirz/shade/hidden/HiddenAppsSearchAlgorithm.java
+++ shade/src/amirz/shade/hidden/HiddenAppsSearchAlgorithm.java
@@ -3,22 +3,50 @@ package amirz.shade.hidden;
 import android.content.Context;
 
 import com.android.launcher3.AppInfo;
+import com.android.launcher3.R;
 import com.android.launcher3.allapps.search.AllAppsSearchBarController;
 import com.android.launcher3.allapps.search.DefaultAppSearchAlgorithm;
+import com.android.launcher3.util.ComponentKey;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class HiddenAppsSearchAlgorithm extends DefaultAppSearchAlgorithm {
     private final Context mContext;
+    private final List<AppInfo> mApps;
+    private final String mKeyword;
 
     public HiddenAppsSearchAlgorithm(Context context, List<AppInfo> apps) {
         super(apps);
         mContext = context;
+        mApps = apps;
+        mKeyword = context.getString(R.string.search_hidden).toLowerCase();
     }
 
     @Override
     public void doSearch(final String query,
                          final AllAppsSearchBarController.Callbacks callback) {
-        super.doSearch(query.trim(), callback);
+        String trimmed = query.trim();
+        boolean showHidden = trimmed.toLowerCase().equals(mKeyword);
+        if (showHidden) {
+            HiddenAppsDrawerState.getInstance(mContext).setRevealed(true);
+        }
+        final ArrayList<ComponentKey> result = getTitleMatchResult(query, showHidden);
+        mResultHandler.post(() -> callback.onSearchResult(query, result));
+    }
+
+    private ArrayList<ComponentKey> getTitleMatchResult(String query, boolean showHidden) {
+        // Do an intersection of the words in the query and each title, and filter out all the
+        // apps that don't match all of the words in the query.
+        final String queryTextLower = query.toLowerCase();
+        final ArrayList<ComponentKey> result = new ArrayList<>();
+        StringMatcher matcher = StringMatcher.getInstance();
+        for (AppInfo info : mApps) {
+            if (matches(info, queryTextLower, matcher) || (showHidden
+                    && HiddenAppsDatabase.isHidden(mContext, info.componentName, info.user))) {
+                result.add(info.toComponentKey());
+            }
+        }
+        return result;
     }
 }
